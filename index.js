24a89076bf385724e97273fdf056365421818c14
Gsherman1 <gsherman@hawaii.edu>
2020-03-03 03:44:46 +0000

    Added Wind & Humidity

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res) {
     
     // takes in the zip from the html form, display in // console. Takes in as string, ex. for zip 02139
-        var zip = String(req.body.zipInput);;
-        console.log(req.body.zipInput);
+        var city = String(req.body.cityInput);;
+        console.log(req.body.cityInput);
     
     //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
         const units = "imperial";
-        const apiKey = "67f6b382921c1e89b39b20d4f9556f22";
-        const url = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip +  "&units=" + units + "&APPID=" + apiKey;
+        const apiKey = "daff48f4973b01c5b2f0a2c732afafc2";
+        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey;
     
     // this gets the data from Open WeatherPI
     https.get(url, function(response){
@@ -31,6 +31,8 @@ app.post("/", function(req, res) {
         response.on("data", function(data){
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
+            const wind = weatherData.wind.speed;
+            const humidity = weatherData.main.humidity;
             const city = weatherData.name;
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
@@ -38,7 +40,8 @@ app.post("/", function(req, res) {
             
             // displays the output of the results
             res.write("<h1> The weather is " + weatherDescription + "<h1>");
-            res.write("<h2>The Temperature in " + city + " " + zip + " is " + temp + " Degrees Fahrenheit<h2>");
+            res.write("<h2>The Wind Speed in " + city + " is " + wind + "mph" + "<h2>");
+            res.write("<h2>The humidity is  " + humidity + "%" + "<h2>");
             res.write("<img src=" + imageURL +">");
             res.send();
         });
